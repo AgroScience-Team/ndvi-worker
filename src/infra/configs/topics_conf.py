a5e740779147f81bad870661367c9ff93a941c37
Alexander Avershin <55056382+asavershin@users.noreply.github.com>
2024-08-27 20:48:57 +0300

    Add topics creation and docs (#2)
    * Add topics config
    
    * Add extensions and paths for aws
    
    * Some docker fixes
    
    * Fix audit file, fix not supported case
    
    * Add doc
    
    * Add attention to docs

---
+++ src/infra/configs/topics_conf.py
@@ -0,0 +1,24 @@
+from ioc.anotations.beans.component import Component
+from ioc.kafka.kafka_conf import KafkaConf
+from ioc.kafka.topics.new_topic import Topic
+
+
+@Component()
+class NdviTopic(Topic):
+
+    def __init__(self, conf: KafkaConf):
+        super().__init__(conf, "ndvi")
+
+
+@Component()
+class WorkersResultsTopic(Topic):
+
+    def __init__(self, conf: KafkaConf):
+        super().__init__(conf, "workers.results")
+
+
+@Component()
+class AuditTopic(Topic):
+
+    def __init__(self, conf: KafkaConf):
+        super().__init__(conf, "agroscienceteam.audit.messages")
